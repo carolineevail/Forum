af5fa7abb878b3f04241c343bc0704540f32be2b
carolineevail <carolineevail@gmail.com>
2016-02-15 10:10:53 -0500

    Refactor code into separate methods

--- src/Forum.java
+++ src/Forum.java
@@ -8,9 +8,23 @@ import java.util.Scanner;
  */
 public class Forum {
     public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<Post> posts = new ArrayList<>();
+        ArrayList<Post> posts = readFile();
 
-        // read all the posts into memory
+        Scanner consoleScanner = new Scanner(System.in);
+
+        int replyId = -1;
+        while (true) {
+
+            //loop over posts and print the ones with the right reply id
+            printPosts(posts, replyId);
+            // ask the use to type a new reply id
+            System.out.println("Type the id you want to see replies to:");
+            replyId = Integer.valueOf(consoleScanner.nextLine());
+        }
+    }
+
+    public static ArrayList<Post> readFile() throws FileNotFoundException {
+        ArrayList<Post> posts = new ArrayList<>();
         File f = new File("posts.txt");
         Scanner fileScanner =  new Scanner(f);
         while (fileScanner.hasNext()) {
@@ -19,24 +33,16 @@ public class Forum {
             Post post = new Post(Integer.valueOf(columns[0]),columns[1], columns[2]);
             posts.add(post);
         }
+        return posts;
+    }
 
-        Scanner consoleScanner = new Scanner(System.in);
-
-        int replyId = -1;
-        while (true) {
-
-            //loop over posts and print the ones with the right reply id
-            int id = 0;
-            for (Post post : posts) {
-                if (post.replyId == replyId) {
-                    System.out.printf("(%d) %s by %s\n", id, post.text, post.author);
-                }
-                id++;
+    public static void printPosts(ArrayList<Post> posts, int replyId) {
+        int id = 0;
+        for (Post post : posts) {
+            if (post.replyId == replyId) {
+                System.out.printf("(%d) %s by %s\n", id, post.text, post.author);
             }
-
-            // ask the use to type a new reply id
-            System.out.println("Type the id you want to see replies to:");
-            replyId = Integer.valueOf(consoleScanner.nextLine());
+            id++;
         }
     }
 }
